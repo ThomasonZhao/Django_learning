3f02c7913fe9e45f776788034454b6f7d42ce17f
Thomason <ThomasonZhao@outlook.com>
2020-04-14 17:01:52 +0800

    update

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,9 +3,10 @@
 @contact: ThomasonZhao@outlook.com 
 @create: 2020/4/12 3:44 PM 
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from booktest import views
 
 urlpatterns = [
-    path('hog/', views.index)
+    path('', views.index),
+    re_path('^(\d+)/$', views.detail),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -10,3 +10,9 @@ def index(request):
     list = models.BookInfo.objects.all()
     context = {'booklist': list}
     return render(request, 'booktest/index2.html', context)
+
+
+def detail(request, id):
+    list = models.BookInfo.objects.get(id=id).heroinfo_set.all()
+    context = {'herolist': list}
+    return render(request, 'booktest/detail.html', context)
