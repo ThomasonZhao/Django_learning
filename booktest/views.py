3a9f005d86073990700cfe9d71b712a2f9ce92e7
Thomason <ThomasonZhao@outlook.com>
2020-04-14 12:14:16 +0800

    update

--- booktest/views.py
+++ booktest/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from booktest import models
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello World')
-    return render(request, 'booktest/index.html')
+    # return render(request, 'booktest/index.html')
+    list = models.BookInfo.objects.all()
+    context = {'booklist': list}
+    return render(request, 'booktest/index2.html', context)
